1454cd2c237c2db05ebf722a56980f3a461651c7
a0979470582 <a0979470582@gmail.com>
2023-03-04 03:55:25 +0800

    chore: apply ts path alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': '.',
+          '@src': './src',
+          tests: ['./tests/'],
+          '@components': './src/components',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {Text} from 'react-native';
+
+export function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Text>Hello World</Text>
+    </NavigationContainer>
+  );
+}
