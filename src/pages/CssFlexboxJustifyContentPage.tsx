5acbd075b85d35a66a98d424df21abba9b5efd02
a0979470582 <a0979470582@gmail.com>
2023-03-04 16:53:00 +0800

    feat: implement CssFlexboxJustifyContentPage

--- src/pages/CssFlexboxJustifyContentPage.tsx
+++ src/pages/CssFlexboxJustifyContentPage.tsx
@@ -1,5 +1,54 @@
-import React from 'react';
+import {BoxFactory, BoxType} from '@src/components/Box/BoxFactory';
+import React, {useState} from 'react';
+import {StyleSheet, View, FlexStyle} from 'react-native';
+import {colors} from '@src/styles/colors';
+import {RadioButtons} from '@src/components/RadioButton/RadioButtons';
+
+const JustifyContents = [
+  'flex-start',
+  'flex-end',
+  'center',
+  'space-between',
+  'space-around',
+  'space-evenly',
+];
 
 export function CssFlexboxJustifyContentPage(): JSX.Element {
-  return <></>;
+  const [index, setIndex] = useState(0);
+  const justifyContent = JustifyContents[index] as FlexStyle['justifyContent'];
+
+  return (
+    <View style={styles.container}>
+      <View style={[styles.boxesContainer, {justifyContent}]}>
+        <BoxFactory boxType={BoxType.Type1} />
+        <BoxFactory boxType={BoxType.Type2} />
+        <BoxFactory boxType={BoxType.Type3} />
+        <BoxFactory boxType={BoxType.Type4} />
+        <BoxFactory boxType={BoxType.Type5} />
+        <BoxFactory boxType={BoxType.Type6} />
+        <BoxFactory boxType={BoxType.Type7} />
+      </View>
+      <RadioButtons
+        titles={JustifyContents}
+        index={index}
+        setIndex={setIndex}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 16,
+    flex: 1,
+  },
+  boxesContainer: {
+    margin: 16,
+    flex: 1,
+    flexWrap: 'wrap',
+    maxHeight: '50%',
+    borderRadius: 12,
+    borderWidth: 8,
+    borderColor: colors.pink_100,
+  },
+});
