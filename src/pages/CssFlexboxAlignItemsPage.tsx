9565c7572eca72ef1c65361c873b77f5978a884c
a0979470582 <a0979470582@gmail.com>
2023-03-04 17:03:20 +0800

    feat: implement CssFlexboxAlignItemsPage

--- src/pages/CssFlexboxAlignItemsPage.tsx
+++ src/pages/CssFlexboxAlignItemsPage.tsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import {BoxFactory, BoxType} from '@src/components/Box/BoxFactory';
+import React, {useState} from 'react';
+import {StyleSheet, View, FlexStyle} from 'react-native';
+import {colors} from '@src/styles/colors';
+import {RadioButtons} from '@src/components/RadioButton/RadioButtons';
+
+const AlignItems = ['flex-start', 'flex-end', 'center', 'stretch', 'baseline'];
 
 export function CssFlexboxAlignItemsPage(): JSX.Element {
-  return <></>;
+  const [index, setIndex] = useState(0);
+  const alignItem = AlignItems[index] as FlexStyle['alignItems'];
+
+  return (
+    <View style={styles.container}>
+      <View style={[styles.boxesContainer, {alignItems: alignItem}]}>
+        <BoxFactory boxType={BoxType.Type1} />
+        <BoxFactory boxType={BoxType.Type2} />
+        <BoxFactory boxType={BoxType.Type3} />
+      </View>
+      <RadioButtons titles={AlignItems} index={index} setIndex={setIndex} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 16,
+    flex: 1,
+  },
+  boxesContainer: {
+    margin: 16,
+    flex: 1,
+    maxHeight: '50%',
+    borderRadius: 12,
+    borderWidth: 8,
+    borderColor: colors.pink_100,
+  },
+});
